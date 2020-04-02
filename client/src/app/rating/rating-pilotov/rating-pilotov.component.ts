98106563e30864f2ad2c8e0bd99da30818dd1c3e
Dmitriy <dimusy@mail.ru>
2020-04-02 15:57:33 +0300

    reting pilotov add sort by date

--- client/src/app/rating/rating-pilotov/rating-pilotov.component.ts
+++ client/src/app/rating/rating-pilotov/rating-pilotov.component.ts
@@ -28,7 +28,21 @@ export class RatingPilotovComponent implements OnInit {
     this.repository.getRatingPilotov().subscribe((rec: any) => {
       this.ratingPilotov = rec;
       this.ratingPilota = this.ratingPilotov.filter(rec => rec.MemberID == this.memberID);
-      console.log(this.ratingPilota);
+      for (let i = 0; i < this.ratingPilota[0].League.length; i++) {
+          this.ratingPilota[0].League[i].Tournaments = this.ratingPilota[0].League[i].Tournaments.sort((n1, n2) => {
+            if (n1.TournamentDate > n2.TournamentDate) {
+              return 1;
+            }
+            if (n1.TournamentDate < n2.TournamentDate) {
+              return -1;
+            }
+            return 0;
+          });
+        for (let j = 0; j < this.ratingPilota[0].League[i].Tournaments.length; j++) {
+          const date = new Date(this.ratingPilota[0].League[i].Tournaments[j].TournamentDate);
+          console.log(date);
+        }
+      }
     });
   }
 }
